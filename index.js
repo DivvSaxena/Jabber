c23744b7c3fa03ca28455dfb171daa4e04f34b77
DivvSaxena <saxenadivv@gmail.com>
2024-02-22 14:23:41 +0530

    features added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase ,ref , push , onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: 'https://jabber-37173-default-rtdb.asia-southeast1.firebasedatabase.app/'
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const chatsInDB = ref(database, "chats")
+
 let loginFormEl = document.getElementById('login-form')
 let usernameEl = document.getElementById('username')
 let loggedOutViewEl = document.getElementById('logged-out-view')
@@ -6,6 +17,8 @@ let messageInputEl = document.getElementById('message-input')
 let jabBtnEl = document.getElementById('jabBtn')
 let chatsEl = document.getElementById('chats')
 let user = ''
+
+
 
 loginFormEl.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -19,27 +32,61 @@ loginFormEl.addEventListener('submit', (e) => {
     clearInputField(usernameEl)
 
     showLoggedInView()
+    scrollDown()
 })
 
 
 
 jabBtnEl.addEventListener('click',(e) => {
     if(messageInputEl.value){
-        chatsEl.innerHTML += `<div class="message-chat">
-                                <p>${messageInputEl.value}</p>
-                                <h4>@${user}</h4>
-                             </div>`
+        let item = {
+            username: `${user}`,
+            chat:`${messageInputEl.value}`
+        }
+        push(chatsInDB, item)
+        
 
-       chatsEl.scrollTop = chatsEl.scrollHeight;
+        scrollDown()
     }
 
     clearInputField(messageInputEl)
 })
 
 
+onValue(chatsInDB , (snapshot) => {
+    
+        let itemsArray = Object.entries(snapshot.val())
+        let feedHtml = ''
+
+        for(let i = 0 ; i < itemsArray.length ; i++){
+            let currentItem = itemsArray[i]
+            let currentItemValue = currentItem[1]
+
+            feedHtml += getFeed(currentItemValue)
+        }
+        render(feedHtml)
+    
+    
+})  
+
+
+function getFeed(item){
+    return `<div class="message-chat">
+                <p>${item.chat}</p>
+                <h4>@${item.username}</h4>
+            </div>`
+}
+
+function render(feedHtml){
+    chatsEl.innerHTML = feedHtml
+}
+
+
 function clearInputField(field){
     field.value = ""
 }
+
+
 
 function showLoggedOutView(){
     hideView(loggedInViewEl)
@@ -57,4 +104,10 @@ function showView(view){
 
 function hideView(view){
     view.style.display = 'none'
+}
+
+
+
+function scrollDown(){
+    chatsEl.scrollTop = chatsEl.scrollHeight;
 }
